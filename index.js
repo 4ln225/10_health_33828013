fbb1c3ec72f5b14f739fcdfd273bfba2b331b6a6
4ln225 <alnstudys@gmail.com>
2025-12-11 21:46:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,54 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-const workoutRoutes = require('./routes/workouts');
-const goalRoutes = require('./routes/goals');
-
+const mysqlStore = require('express-mysql-session')(session);
 require('dotenv').config();
 
-// import routes
 const mainRoutes = require('./routes/main');
 const authRoutes = require('./routes/auth');
+const workoutRoutes = require('./routes/workouts');
+const goalsRoutes = require('./routes/goals');
 
+// Create app
 const app = express();
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Sessions
+const sessionStore = new mysqlStore({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+app.use(
+    session({
+        secret: 'pulseprosecret',
+        resave: false,
+        saveUninitialized: false,
+        store: sessionStore
+    })
+);
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// View engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-
-
-app.use(
-  session({
-    secret: 'pulsepro-secret',
-    resave: false,
-    saveUninitialized: false
-  })
-);
-
-// Flash , user injection middleware 
-app.use((req, res, next) => {
-  res.locals.currentUser = req.session.user || null;
-  res.locals.flash = req.session.flash || null;
-  delete req.session.flash; // only 1 time
-  next();
-});
-
-// register route files
+// Routes
 app.use('/', mainRoutes);
 app.use('/', authRoutes);
 app.use('/workouts', workoutRoutes);
-app.use('/goals', goalRoutes);
+app.use('/goals', goalsRoutes);
 
-
-
-// 404 handler helps know what page aint wokrin
-app.use((req, res) => {
-  res.status(404).render('404', { title: "Not Found" });
-});
-
-// start server
-const PORT = process.env.PORT || 58398;
-
+// Start server on VM-specific WWW port
+const PORT = 8398;
 app.listen(PORT, () => {
-  console.log(`PulsePro running on http://localhost:${PORT}`);
+    console.log(`PulsePro running on: http://localhost:${PORT}`);
 });
+
