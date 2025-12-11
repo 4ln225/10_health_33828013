9a9d1139c82f5f9c8e3d9d68568040b9feeda55a
4ln225 <alnstudys@gmail.com>
2025-12-11 21:21:24 +0000

    Update insert_test_data.sql

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -1,17 +1,13 @@
-USE health;
-
--- sample user (gold / smiths) 
-INSERT INTO users (username, password_hash) VALUES 
-('gold', '$2b$10$u9tXaj1FqGV8ho99j/BmveUL5ny3Vx9gq8k8spcB7GxeF3e0mra7e');
+INSERT INTO users (username, password_hash)
+VALUES ('gold', '$2b$10$u9tXaj1FqGV8ho99j/BmveUL5ny3Vx9gq8k8spcB7GxeF3e0mra7e');
 
 -- sample workouts 
 INSERT INTO workouts (user_id, workout_date, workout_type, duration_minutes, intensity, notes) VALUES
-(1, '2025-01-05', 'Run', 20, 'medium', 'Quick morning run before lectures'),
-(1, '2025-01-08', 'Gym', 45, 'high', 'Chest day'),
-(1, '2025-01-10', 'Yoga', 30, 'low', 'Daily stretches);
+(1, '2025-01-05', 'Run', 20, 'medium', 'Morning chest day'),
+(1, '2025-01-08', 'Gym', 45, 'high', 'Chest day in gym'),
+(1, '2025-01-10', 'Yoga', 30, 'low', 'Daily stretches session');
 
 -- sample goals 
 INSERT INTO goals (user_id, title, target_value, unit, period) VALUES
-(1, 'Do more stretching (yoga) to avoid injuries', 4, 'sessions', 'per week'),
-(1, 'Increase running distance gradually', 3, 'runs', 'per week');
-
+(1, 'Do more stretching yoga to avoid injuries', 4, 'sessions', 'per week'),
+(1, 'Increase running distance', 3, 'kilometres', 'per week');
