a0b3fed4519cc286685acaf012e02daebdc272e4
4ln225 <alnstudys@gmail.com>
2025-12-13 19:34:01 +0000

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,66 +4,85 @@ const pool = require('../config/db');
 
 const router = express.Router();
 
-// Login form
+
+// login form
+
 router.get('/login', (req, res) => {
-  res.render('/login', { title: "Login" });
+  res.render('login', { title: 'Login' });
 });
 
-// Login submit
-router.post('login', async (req, res) => {
+
+router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   const [rows] = await pool.query(
-    "SELECT * FROM users WHERE username = ?",
+    'SELECT * FROM users WHERE username = ?',
     [username]
   );
 
   if (rows.length === 0) {
-    req.session.flash = { type: 'error', message: 'Invalid username or password.' };
-    return res.redirect('/usr/398/login');
+    req.session.flash = {
+      type: 'error',
+      message: 'Invalid username or password.'
+    };
+    return res.redirect('login');
   }
 
   const user = rows[0];
   const match = await bcrypt.compare(password, user.password_hash);
 
   if (!match) {
-    req.session.flash = { type: 'error', message: 'Invalid username or password.' };
-    return res.redirect('/login');
+    req.session.flash = {
+      type: 'error',
+      message: 'Invalid username or password.'
+    };
+    return res.redirect('login');
   }
 
   req.session.user = { id: user.id, username: user.username };
-  res.redirect('/workouts');
+  res.redirect('workouts');
 });
 
-// Register form
+
+// Rgister form
+
 router.get('/register', (req, res) => {
-  res.render('/register', { title: "Register" });
+  res.render('register', { title: 'Register' });
 });
 
-// Register submit
+
 router.post('/register', async (req, res) => {
   const { username, password } = req.body;
   const hash = await bcrypt.hash(password, 10);
 
   try {
     await pool.query(
-      "INSERT INTO users (username, password_hash) VALUES (?, ?)",
+      'INSERT INTO users (username, password_hash) VALUES (?, ?)',
       [username, hash]
     );
 
-    req.session.flash = { type: 'success', message: 'Account created. Please log in.' };
-    res.redirect('/usr/398/login');
-  } catch {
-    req.session.flash = { type: 'error', message: 'Username already exists.' };
-    res.redirect('/usr/398/register');
+    req.session.flash = {
+      type: 'success',
+      message: 'Account created. Please log in.'
+    };
+
+    res.redirect('login');
+  } catch (err) {
+    req.session.flash = {
+      type: 'error',
+      message: 'Username already exists.'
+    };
+    res.redirect('register');
   }
 });
 
-// Logout
+//logout
+
 router.get('/logout', (req, res) => {
   req.session.destroy(() => {
-    res.redirect('/usr/398/login');
+    res.redirect('login');
   });
 });
 
 module.exports = router;
+
