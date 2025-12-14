495bb70e507809daafa604cc8ec865ce5254603a
4ln225 <alnstudys@gmail.com>
2025-12-14 00:41:05 +0000

    Update create_db.sql

--- db/create_db.sql
+++ db/create_db.sql
@@ -20,14 +20,3 @@ CREATE TABLE workouts (
 );
 
 
--- woals table
-CREATE TABLE IF NOT EXISTS goals (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  title VARCHAR(100) NOT NULL,
-  target_value INT NOT NULL,
-  unit VARCHAR(50) NOT NULL,
-  period VARCHAR(50) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
